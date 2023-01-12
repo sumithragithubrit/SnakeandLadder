48015941abe6848c8c776ac5e304a071611ff836
sumi <sumipavib1302@gmail.com>
2023-01-12 14:33:40 +0530

    Snake and ladder initial from zero

---
+++ SnakeAndLadder.java
@@ -0,0 +1,17 @@
+package com.snakeandladder;
+
+
+public class SnakeAndLadder {
+	static final int initialPosition = 0;
+	
+	public static void main(String[] args) {
+	System.out.println("Welcome in snake ladder program");
+    System.out.println("current position "+initialPosition);
+
+    }
+
+
+
+    
+
+}
