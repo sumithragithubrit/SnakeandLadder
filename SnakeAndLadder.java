06f7fa938ab5a90cbe9fe500e76ce9ebcdb632cb
sumi <sumipavib1302@gmail.com>
2023-01-12 16:51:20 +0530

    rolls the die to get number

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,12 +1,16 @@
 package com.snakeandladder;
-
+import java.util.Random;
 
 public class SnakeAndLadder {
 	static final int initialPosition = 0;
 	
 	public static void main(String[] args) {
 	System.out.println("Welcome in snake ladder program");
-    System.out.println("current position "+initialPosition);
+	Random random = new Random();
+    int diceRoll = random.nextInt(7);
+
+    System.out.println("current position "+diceRoll);
+    
 
     }
 
