d1b4002e51f86b0ffc3f36e537904fbe2525f49e
sumi <sumipavib1302@gmail.com>
2023-01-13 10:17:10 +0530

    exact winning position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,12 +3,17 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 	static  int initialPosition = 0;
-	static final  int ladder=1;
-    static final int snake=2;
+	static final  int ladder=2;
+    static final int snake=1;
     static final int noPlay=0;
+    static public final int Starting_Position_Of_Player = 0;
+    static public final int Ending_Position_Of_Player = 100;
+    static int count = 0;
+    
     
     static int rollDice(){
     	int dieValue =(int)Math.floor(Math.random()*10)%6+1;
+    	count++;
         return dieValue;
     }
 	
@@ -19,27 +24,39 @@ public class SnakeAndLadder {
 		System.out.println("current position "+diceRoll);
 		int rollDice=rollDice();
 		
-    int checkPosition= (int)Math.floor(Math.random()*10)%3;
-    System.out.println(checkPosition);
        
-    switch (checkPosition){
-        case snake:
-            initialPosition=rollDice-initialPosition;
-            System.out.println("player is on snake" +initialPosition);
-            break;
-        case  ladder:
-            initialPosition=ladder+rollDice;
-            System.out.println("player is on Ladder " +initialPosition);
-            break;
-        default:
-            System.out.println("player is not play " +initialPosition);
-    }
+        int presentPlayerPosition = 0;
+        while (presentPlayerPosition < Ending_Position_Of_Player) {
+            int currentPosition = 0;
+            int checkPosition = (int) (Math.random() * 10) % 3;
+            System.out.println(checkPosition);
+            
+            switch (checkPosition) {
+                case snake:
+                    currentPosition =  rollDice();
+                    if ((presentPlayerPosition-currentPosition)>=0 ) {
+                    	presentPlayerPosition -= currentPosition;
+                    	System.out.println("player is on snake " +presentPlayerPosition);
+                    }
+                    break;
+                case ladder:
+                    currentPosition =  rollDice();
+                    if((presentPlayerPosition+currentPosition)<=100 ) {
+                    presentPlayerPosition += currentPosition;
+                    System.out.println("player is on Ladder " +presentPlayerPosition);
+                    }
+                    System.out.println("Number of time rollDie  "+count);
+                    
+                    break;
+                default:
+                    System.out.println("player is not play ");
+                                     }
     
 
-    }
+                   }
 
 
 
     
-
+	}
 }
