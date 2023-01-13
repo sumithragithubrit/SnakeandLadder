81721601b89b94d625d330005c7925ce10bbf6af
sumi <sumipavib1302@gmail.com>
2023-01-13 11:28:02 +0530

    added:game with two players

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,54 +9,84 @@ public class SnakeAndLadder {
     static public final int Starting_Position_Of_Player = 0;
     static public final int Ending_Position_Of_Player = 100;
     static int count = 0;
+	static int presentPlayerPosition1 = 0;
+	static int presentPlayerPosition2 = 0;
+	int p1CurrentPosition = 0;
+	int p2CurrentPosition = 0;
     
     
-    static int rollDice(){
+   public static int rollDice(){
     	int dieValue =(int)Math.floor(Math.random()*10)%6+1;
     	count++;
         return dieValue;
     }
+   public static void player() {
+		int player1 = 0;
+		int player2 = 1;
+
+	}
+   
+   public void player1(int checkPosition){
+		switch (checkPosition) {
+			case snake:
+				p1CurrentPosition = rollDice();
+				if ((presentPlayerPosition1 - p1CurrentPosition) >= 0) {
+					presentPlayerPosition1 -= p1CurrentPosition;
+					System.out.println("player1 is on snake " + presentPlayerPosition1);
+				}
+				break;
+			case ladder:
+				p1CurrentPosition = rollDice();
+				if ((presentPlayerPosition1 + p1CurrentPosition) <= 100) {
+					presentPlayerPosition1 += p1CurrentPosition;
+					System.out.println("player1 is on Ladder " + presentPlayerPosition1);
+				}
+				break;
+			default:
+				System.out.println("player1 is not play ");
+		}
+	}
+
+   public void player2(int checkPosition){
+	switch (checkPosition) {
+		case snake:
+			p2CurrentPosition = rollDice();
+			if ((presentPlayerPosition2 - p2CurrentPosition) >= 0) {
+				presentPlayerPosition2 -= p2CurrentPosition;
+				System.out.println("player2 is on snake " + presentPlayerPosition2);
+				System.out.println("Number of time rollDie  "+count);
+			}
+			break;
+		case ladder:
+			p2CurrentPosition = rollDice();
+			if ((presentPlayerPosition2 + p2CurrentPosition) <= 100) {
+				presentPlayerPosition2 += p2CurrentPosition;
+				System.out.println("player2 is on Ladder " + presentPlayerPosition2);
+				System.out.println("Number of time rollDie  "+count);
+			}
+			break;
+		default:
+			System.out.println("player is not play ");
+		}
+			System.out.println("player2 is not play ");
+	}
+		
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome in snake ladder program");
-		Random random = new Random();
-		int diceRoll = random.nextInt(7);
-		System.out.println("current position "+diceRoll);
-		int rollDice=rollDice();
-		
-       
-        int presentPlayerPosition = 0;
-        while (presentPlayerPosition < Ending_Position_Of_Player) {
-            int currentPosition = 0;
-            int checkPosition = (int) (Math.random() * 10) % 3;
-            System.out.println(checkPosition);
-            
-            switch (checkPosition) {
-                case snake:
-                    currentPosition =  rollDice();
-                    if ((presentPlayerPosition-currentPosition)>=0 ) {
-                    	presentPlayerPosition -= currentPosition;
-                    	System.out.println("player is on snake " +presentPlayerPosition);
-                    }
-                    break;
-                case ladder:
-                    currentPosition =  rollDice();
-                    if((presentPlayerPosition+currentPosition)<=100 ) {
-                    presentPlayerPosition += currentPosition;
-                    System.out.println("player is on Ladder " +presentPlayerPosition);
-                    }
-                    System.out.println("Number of time rollDie  "+count);
-                    
-                    break;
-                default:
-                    System.out.println("player is not play ");
-                                     }
-    
+		 SnakeAndLadder sal = new SnakeAndLadder ();
+		while (presentPlayerPosition1 < Ending_Position_Of_Player || presentPlayerPosition2 < Ending_Position_Of_Player) {
+			int checkPosition = (int) (Math.random() * 10) % 3;
+			if(count%2==0)
+				sal.player1(checkPosition);
+			else
+				sal.player2(checkPosition);
 
-                   }
+			count++;
 
 
 
     
-	}
+	      }
+    }
 }
